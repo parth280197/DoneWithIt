92fda07e01d6e628dd81646fb15fa01c4640dcf2
Parth Patel <parth280197@gmail.com>
2021-07-24 14:36:40 -0500

    Add AppTextInput component.

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native';
+import AppTextInput from './app/components/inputs/AppTextInput';
 import Screen from './app/components/screens/Screen';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingScreen from './app/screens/ListingScreen';
 
 export default function App() {
   return (
-      <>
-        <ListingScreen />
-        {/* <ListItem title="Parth" subTitle="Patel" ImageComponent={emailIcon} /> */}
-      </>
-    );
+    <SafeAreaView>
+      <AppTextInput icon="email" placeholder="Email" />
+      {/* <ListItem title="Parth" subTitle="Patel" ImageComponent={emailIcon} /> */}
+    </SafeAreaView>
+  );
 };
