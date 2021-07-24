aaaae9db17a62d8ae266e99a3d3dba581b2512fb
Parth Patel <parth280197@gmail.com>
2021-07-24 15:30:04 -0500

    Add AppPicker Component.

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
-import AppTextInput from './app/components/inputs/AppTextInput';
-import Screen from './app/components/screens/Screen';
-import AppText from './app/components/texts/AppText';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingScreen from './app/screens/ListingScreen';
+import React, { useState } from "react";
+
+import Screen from "./app/components/screens/Screen";
+import AppPicker from "./app/components/picker/AppPicker";
+import AppTextInput from "./app/components/inputs/AppTextInput";
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
 
 export default function App() {
+  const [category, setCategory] = useState(categories[0]);
+
   return (
-    <SafeAreaView>
-      <AppText>Test</AppText>
-      {/* <ListItem title="Parth" subTitle="Patel" ImageComponent={emailIcon} /> */}
-    </SafeAreaView>
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
   );
-};
+}
---
+++ app/components/picker/AppPicker.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import {
+    View,
+    StyleSheet,
+    TouchableWithoutFeedback,
+    Modal,
+    Button,
+    FlatList,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import AppText from "./../texts/AppText";
+import Screen from "./../screens/Screen";
+import defaultStyles from "../../utils/styles";
+import PickerItem from "./PickerItem";
+
+function AppPicker({ icon, items, onSelectItem, placeholder, selectedItem }) {
+    const [modalVisible, setModalVisible] = useState(false);
+
+    return (
+        <>
+            <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+                <View style={styles.container}>
+                    {icon && (
+                        <MaterialCommunityIcons
+                            name={icon}
+                            size={20}
+                            color={defaultStyles.colors.medium}
+                            style={styles.icon}
+                        />
+                    )}
+                    <AppText style={styles.text}>
+                        {selectedItem ? selectedItem.label : placeholder}
+                    </AppText>
+                    <MaterialCommunityIcons
+                        name="chevron-down"
+                        size={20}
+                        color={defaultStyles.colors.medium}
+                    />
+                </View>
+            </TouchableWithoutFeedback>
+            <Modal visible={modalVisible} animationType="slide">
+                <Screen>
+                    <Button title="Close" onPress={() => setModalVisible(false)} />
+                    <FlatList
+                        data={items}
+                        keyExtractor={(item) => item.value.toString()}
+                        renderItem={({ item }) => (
+                            <PickerItem
+                                label={item.label}
+                                onPress={() => {
+                                    setModalVisible(false);
+                                    onSelectItem(item);
+                                }}
+                            />
+                        )}
+                    />
+                </Screen>
+            </Modal>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: defaultStyles.colors.light,
+        borderRadius: 25,
+        flexDirection: "row",
+        width: "100%",
+        padding: 15,
+        marginVertical: 10,
+    },
+    icon: {
+        alignSelf: 'center',
+        marginRight: 10,
+    },
+    text: {
+        flex: 1,
+    },
+});
+
+export default AppPicker;
