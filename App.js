7612f6a3f8ac01f6815fa9f21d3b877efe8c8de3
Parth Patel <parth280197@gmail.com>
2021-07-24 14:40:04 -0500

    Improve AppText component

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { SafeAreaView } from 'react-native';
 import AppTextInput from './app/components/inputs/AppTextInput';
 import Screen from './app/components/screens/Screen';
+import AppText from './app/components/texts/AppText';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingScreen from './app/screens/ListingScreen';
 
 export default function App() {
   return (
     <SafeAreaView>
-      <AppTextInput icon="email" placeholder="Email" />
+      <AppText>Test</AppText>
       {/* <ListItem title="Parth" subTitle="Patel" ImageComponent={emailIcon} /> */}
     </SafeAreaView>
   );
